1ee319bd335b3258cabe24724660334184a7f542
martipurull <91315241+martipurull@users.noreply.github.com>
2021-10-26 17:34:05 +0200

    all but last exercise completed

--- m3_d2.js
+++ m3_d2.js
@@ -44,21 +44,58 @@ window.onload = () => {
         .then(receivedData => {
             console.log(receivedData)
             const centreSection = document.querySelector('.centre-section-container')
-            const newSection = document.createElement('div')
-            newSection.classList.add('container-fluid')
-            newSection.classList.add('d-flex')
-            centreSection.prepend(newSection)
             const sectionTitle = document.createElement('h4')
             sectionTitle.classList.add('py-3')
             sectionTitle.innerHTML = "<strong>The Latest Post-Punk</strong>"
-            newSection.appendChild(sectionTitle)
+            centreSection.prepend(sectionTitle)
+            const newSection = document.createElement('div')
+            newSection.classList.add('container-fluid')
+            newSection.classList.add('d-flex')
+            newSection.classList.add('flex-wrap')
+            sectionTitle.insertAdjacentElement('afterend', newSection)
+            const countAlbumsDiv = document.createElement('div')
+            countAlbumsDiv.classList.add('my-3')
+            countAlbumsDiv.innerHTML = `
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                        Count Albums
+          </button>
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                    aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title text-dark" id="exampleModalLabel">Number of albums in section</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                             ${ receivedData.data.length }
+                </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `
+            newSection.insertAdjacentElement('beforebegin', countAlbumsDiv)
+            const countUniqueButton = document.createElement('button')
+            countUniqueButton.innerText = 'Count Unique Albums'
+            countUniqueButton.classList.add('btn')
+            countUniqueButton.classList.add('btn-secondary')
+            countUniqueButton.addEventListener('click', countUnique)
+            countAlbumsDiv.insertAdjacentElement('afterbegin', countUniqueButton)
             for (let i = 0; i < receivedData.data.length; i++) {
                 const cardContainer = document.createElement('div')
                 cardContainer.classList.add('card')
+                cardContainer.classList.add('mx-1')
                 const cardImg = document.createElement('img')
-                cardImg.src = receivedData.data.album.cover
+                cardImg.src = receivedData.data[i].album.cover
                 const cardBody = document.createElement('div')
                 cardBody.classList.add('card-body')
+                cardBody.innerText = receivedData.data[i].album.title
+                cardBody.classList.add('text-white')
                 newSection.appendChild(cardContainer)
                 cardContainer.appendChild(cardImg)
                 cardContainer.appendChild(cardBody)
@@ -67,5 +104,11 @@ window.onload = () => {
         .catch(err => {
             console.error(err);
         });
+
+}
+
+const countUnique = function () {
+    const albumTitles = document.querySelectorAll('.card-body')
+    console.log(albumTitles[0].innerText)
 
 }
